d7c596b5819c3b25ca4b374ec40ac5ddfd60f219
milton-reyes <altf4616@outlook.com>
2020-06-26 17:27:14 -0400

    changes to tier 1

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,26 @@
 const typeOfParamameter = (p) => {
     return typeof(p)
 }
-console.log("Create a function that will return the datatype parameter")
+console.log("1. Create a function that will return the datatype parameter")
 console.log(typeOfParamameter("hello"))
 
 //Create a function that will take in a variable number of arguments and return their product
 const variableNumberArgs = (...arguments) => {
     let product = 1
     arguments.forEach(a => {
+        //product*=a
         console.log(product*=a)
     })
     //OR
     //for(let i =1; i <arguments.length;i++) {
     //    console.log(product*=arguments[i])
     //}
+
+    console.log("returning product from variableNumberArgs")
+    return product
 }
-console.log("Create a function that will take in a variable number of arguments and return their product")
-variableNumberArgs(2,2,3,1,4)
+console.log("2. Create a function that will take in a variable number of arguments and return their product")
+console.log(variableNumberArgs(2,2,3,1,4))
 
 //Create a function that iterates over an integer array and returns their sum
 const returnIntArraySum = () => {
@@ -27,10 +31,11 @@ const returnIntArraySum = () => {
         console.log(a)
         total+=a
     })
-    console.log("total: "+total)
+    console.log("returning integer array sum")
+    return total
 }
-console.log("Create a function that iterates over an integer array and returns their sum")
-returnIntArraySum()
+console.log("3. Create a function that iterates over an integer array and returns their sum")
+console.log(returnIntArraySum())
 
 /*Create a function that, given an integer, can raise each digit to the power of 
 the overall length and return whether the sum of the powers is equal to the original integer*/
@@ -57,6 +62,7 @@ const raiseLengthPower = (integer) => {
     console.log("sum of the powers")
     console.log(sumPowers)
     console.log(sumPowers +" is equal to " + integer)
+    console.log("returning wether the sum of the powers equal the original integer")
     return sumPowers===integer
 }
 console.log(raiseLengthPower(333))
